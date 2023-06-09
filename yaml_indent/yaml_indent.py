f736ab4df91a40c5baf0392de2ba88fde0b6a079
Knut Olav Bøhmer <knut.bohmer@fremtind.no>
2023-06-09 16:30:31 +0200

    Use ruamel.yaml

--- yaml_indent/yaml_indent.py
+++ yaml_indent/yaml_indent.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
 
 import argparse
-import yaml
+from ruamel.yaml import YAML
+
+
+def find_config_file():
+    # Start from the current directory
+    dir_path = os.getcwd()
+    while dir_path != os.path.dirname(dir_path):  # Stop at the root directory
+        config_path = os.path.join(dir_path, 'config.ini')
+        if os.path.exists(config_path):
+            return config_path
+        # Go up to the parent directory
+        dir_path = os.path.dirname(dir_path)
+    return None
+
+
 
 def process_yaml_file(input_file, output_file):
+    yaml = YAML()
     # Open the input YAML file and load the data
     with open(input_file, 'r') as stream:
         try:
-            data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
+            data = yaml.load(stream)
+        except Exception as exc:
             print(exc)
 
-    # Open the output file and dump the data with correct indenting
+
+    config_path = find_config_file()
+    if config_path:
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        mapping = config.getint('YAML', 'mapping', fallback=2)
+        sequence = config.getint('YAML', 'sequence', fallback=4)
+        offset = config.getint('YAML', 'offset', fallback=2)
+    else:
+        mapping, sequence, offset = 2, 4, 2  # Default values
+
+    yaml.indent(mapping=mapping, sequence=sequence, offset=offset)
+
     with open(output_file, 'w') as outfile:
         try:
-            yaml.safe_dump(data, outfile, default_flow_style=False, sort_keys=False)
-        except yaml.YAMLError as exc:
+            yaml.dump(data, outfile)
+        except Exception as exc:
             print(exc)
+
+
 
 def main():
     parser = argparse.ArgumentParser(description="Read a YAML file and write it out with correct indentation")
